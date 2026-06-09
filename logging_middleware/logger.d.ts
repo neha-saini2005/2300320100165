1ae5f4b3950c78bdb2fa14c706ba8dd50997eb08
neha-saini2005 <nehasainidm21@gmail.com>
2026-06-09 12:18:16 +0530

    add logging middleware

---
+++ logging_middleware/logger.d.ts
@@ -0,0 +1,2 @@
+export declare function Log(stack: string, level: string, package_name: string, message: string): Promise<void>;
+//# sourceMappingURL=logger.d.ts.map
---
+++ logging_middleware/logger.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const CLIENT_ID = "4f1cdd6f-e368-4772-8385-dffbb52c0d9f";
+const CLIENT_SECRET = "jXGtzEnHqpqzPdSU";
+const LOG_URL = "http://4.224.186.213/evaluation-service/logs";
+const AUTH_URL = "http://4.224.186.213/evaluation-service/auth";
+
+let token: string = "";
+
+async function getToken(): Promise<void> {
+  const response = await axios.post(AUTH_URL, {
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+  });
+  token = response.data.access_token;
+}
+
+export async function Log(
+  stack: string,
+  level: string,
+  package_name: string,
+  message: string
+): Promise<void> {
+  if (!token) {
+    await getToken();
+  }
+  try {
+    await axios.post(
+      LOG_URL,
+      { stack, level, package: package_name, message },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+  } catch (error) {
+    console.error("Logging failed:", error);
+  }
+}
